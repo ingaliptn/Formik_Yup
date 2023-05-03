d1592da871cbb61d51c7c67dd0c7ddfd2db91ddd
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-03 14:47:42 +0300

    Refactor tags

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,18 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.name}>{label}</label>
+      <input {...props} {...field} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 const CustomForm = () => {
   return (
@@ -28,16 +41,23 @@ const CustomForm = () => {
       onSubmit={(values) => console.log(JSON.stringify(values, null, 2))}>
       <Form className="form">
         <h2>Отправить пожертвование</h2>
-        <label htmlFor="name">Ваше имя</label>
-        <Field id="name" name="name" type="text" />
-        <ErrorMessage className="error" name="name" component="div" />
-        <label htmlFor="email">Ваша почта</label>
-        <Field id="email" name="email" type="email" />
-        <ErrorMessage className="error" name="email" component="div" />
 
-        <label htmlFor="amount">Количество</label>
-        <Field id="amount" name="amount" type="number" />
-        <ErrorMessage className="error" name="amount" component="div" />
+        <MyTextInput label={"Ваше имя"} id="name" name="name" type="text" />
+
+        <MyTextInput
+          label={"Ваша почта"}
+          id="email"
+          name="email"
+          type="email"
+        />
+
+        <MyTextInput
+          label={"Количество"}
+          id="amount"
+          name="amount"
+          type="number"
+        />
+
         <label htmlFor="currency">Валюта</label>
         <Field id="currency" name="currency" as="select">
           <option value="">Выберите валюту</option>
@@ -46,6 +66,7 @@ const CustomForm = () => {
           <option value="EUR">EUR</option>
         </Field>
         <ErrorMessage className="error" name="currency" component="div" />
+
         <label htmlFor="text">Ваше сообщение</label>
         <Field id="text" name="text" as="textarea" />
         <ErrorMessage className="error" name="text" component="div" />
